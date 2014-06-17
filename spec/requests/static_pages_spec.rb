9116dcd99e72e274d6d02cc338f7cfdf487ced3e
Brian Zable <brian@brianzable.com>
2014-06-16 20:58:14 -0500

    finished basic tests for StaticPages pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,5 +6,27 @@ describe "StaticPages", :type => :request do
       visit '/'
       expect(page).to have_content('Welcome to Hivelife!')
     end
+
+    it "should have title 'Hivelife'" do
+      visit '/'
+      expect(page).to have_title('Hivelife')
+    end
+  end
+
+  describe 'Data Portal' do
+    it "should have title 'Hivelife - Data Portal'" do
+      visit '/data'
+      expect(page).to have_title('Hivelife | Data Portal')
+    end
+
+    it "should have links to all CSV files" do
+      visit '/data'
+      expect(page).to have_link('Hives', href: 'https://s3-us-west-2.amazonaws.com/hivelife/dumps/hives.csv')
+      expect(page).to have_link('Inspections', href: 'https://s3-us-west-2.amazonaws.com/hivelife/dumps/inspections.csv')
+      expect(page).to have_link('Brood Boxes', href: 'https://s3-us-west-2.amazonaws.com/hivelife/dumps/brood_boxes.csv')
+      expect(page).to have_link('Honey Supers', href: 'https://s3-us-west-2.amazonaws.com/hivelife/dumps/honey_supers.csv')
+      expect(page).to have_link('Diseases', href: 'https://s3-us-west-2.amazonaws.com/hivelife/dumps/diseases.csv')
+      expect(page).to have_link('Harvests', href: 'https://s3-us-west-2.amazonaws.com/hivelife/dumps/harvests.csv')
+    end
   end
 end
