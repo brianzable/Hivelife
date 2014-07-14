deda61021473429514658b01d5bfd9672f5dda3b
Brian Zable <brian@brianzable.com>
2014-07-14 16:18:12 -0500

    merged add-tests into master

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,10 +9,31 @@ FactoryGirl.define do
   factory :apiary do
     name 'Test Apiary'
     zip_code '60126'
+    city 'My City'
+    state 'IL'
+    zip_code '00000'
+
+    factory :apiary_with_hives do
+      ignore do
+        hives_count 2
+      end
+
+      after(:create) do |apiary, evaluator|
+        create_list(:hive, evaluator.hives_count, apiary: apiary, user_id: apiary.user_id)
+      end
+    end
   end
 
   factory :beekeeper do
-    creator { user.id }
     permission 'Admin'
+  end
+
+  factory :hive do
+    sequence :name do |n|
+      "Hive #{n}"
+    end
+    latitude 88.8888888
+    longitude 88.8888888
+    hive_type 'Langstroth'
   end
 end
--- spec/requests/apiaries_spec.rb
+++ spec/requests/apiaries_spec.rb
@@ -8,25 +8,51 @@ RSpec.describe 'Apiaries', type: :request do
   end
 
   describe 'Apiaries#index' do
-
-    it "should have title 'Hivelife | Home'" do
-      visit root_path
-      should have_title('Hivelife | Home')
+    it "will have title 'Hivelife | Home'" do
+      visit '/'
+      expect(page).to have_title('Hivelife | Home')
     end
 
-    it 'should have the create apiary button' do
-      visit root_path
-      should have_link('+', href: new_apiary_path)
+    it 'will link to apiaries#new' do
+      visit authenticated_root_path
+      expect(page).to have_link('+', href: new_apiary_path)
     end
 
-    it 'should display an apiary name' do
-      apiary = FactoryGirl.create(:apiary)
-      beekeeper = FactoryGirl.create(:beekeeper, user: @user, apiary: apiary)
-
-      visit root_path
-
-      should have_content('Test Apiary')
-      should have_content('0 HIVES')
+    it 'will display hive count and apiary city/state' do
+      apiary = FactoryGirl.create(:apiary, user_id: @user.id)
+      beekeeper = FactoryGirl.create(:beekeeper,
+                                      user: @user,
+                                      apiary: apiary,
+                                      creator: @user.id)
+      visit authenticated_root_path
+      expect(page).to have_selector('h3', text: '0')
+      expect(page).to have_selector('h5', text: 'HIVES')
+      expect(page).to have_selector('h3', text: apiary.name)
+      expect(page).to have_selector('h5', text: "#{apiary.city}, #{apiary.state}")
     end
+
+    it 'will display hive count (2)' do
+      apiary_with_hives = FactoryGirl.create(:apiary_with_hives, user_id: @user.id)
+      beekeeper = FactoryGirl.create(:beekeeper,
+                                      user: @user,
+                                      apiary: apiary_with_hives,
+                                      creator: @user.id)
+      visit authenticated_root_path
+      expect(page).to have_selector('h3', text: '2')
+      expect(page).to have_selector('h5', text: 'HIVES')
+    end
+  end
+
+  describe 'Apiaries#new' do
+    it 'will display errors if the name and zip code are blank' do
+      visit new_apiary_path
+      click_button('Save')
+      expect(page).to have_content('Apiary name cannot be blank')
+      expect(page).to have_content('Zip code cannot be blank')
+    end
+
+    # Check for successful submission
+
+    # Check redirect
   end
 end
