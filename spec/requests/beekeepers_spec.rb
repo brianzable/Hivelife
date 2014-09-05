404b503f668a00396e7ac27f498d97deadc83299
Brian Zable <brian@brianzable.com>
2014-09-05 15:53:04 -0500

    added tests for Beekeepers#update / refactored some old tests

--- spec/requests/beekeepers_spec.rb
+++ spec/requests/beekeepers_spec.rb
@@ -9,16 +9,6 @@ RSpec.describe 'Beekeepers', type: :request do
                                     user: @user,
                                     apiary: @apiary,
                                     creator: @user.id)
-    @beekeeper_json = {
-      id: @beekeeper.id,
-      permission: @beekeeper.permission,
-      apiary_id: @beekeeper.apiary_id,
-      user: {
-        user_id: @user.id,
-        first_name: @user.first_name,
-        last_name: @user.last_name
-      }
-    }.to_json
 
     @http_headers = {
       'Content-Type' => 'application/json',
@@ -29,15 +19,27 @@ RSpec.describe 'Beekeepers', type: :request do
   describe '#show' do
     it 'returns a JSON representation of a beekeeper' do
       get apiary_beekeeper_path(@apiary, @beekeeper), format: :json
+
+      beekeeper_json = {
+        id: @beekeeper.id,
+        permission: @beekeeper.permission,
+        apiary_id: @beekeeper.apiary_id,
+        user: {
+          user_id: @user.id,
+          first_name: @user.first_name,
+          last_name: @user.last_name
+        }
+      }.to_json
+
       expect(response).to be_success
-      expect(response.body).to eq(@beekeeper_json)
+      expect(response.body).to eq(beekeeper_json)
     end
   end
 
   describe '#create' do
     it 'creates a new Beekeeper in the database' do
       new_user = FactoryGirl.create(:user, email: "new_guy@example.com")
-      data = {
+      payload = {
         beekeeper: {
           email: new_user.email,
           permission: 'Read'
@@ -45,7 +47,7 @@ RSpec.describe 'Beekeepers', type: :request do
       }
       original_beek_count = Beekeeper.count
 
-      post(apiary_beekeepers_path(@apiary), data.to_json, @http_headers)
+      post(apiary_beekeepers_path(@apiary), payload.to_json, @http_headers)
 
       expect(response).to be_success
       expect(Beekeeper.count).to be(original_beek_count + 1)
@@ -53,14 +55,14 @@ RSpec.describe 'Beekeepers', type: :request do
 
     it 'returns a JSON representation of the new beekeeper object' do
       new_user = FactoryGirl.create(:user, email: "new_guy@example.com")
-      data = {
+      payload = {
         beekeeper: {
           email: new_user.email,
           permission: 'Read'
         }
       }
 
-      post(apiary_beekeepers_path(@apiary), data.to_json, @http_headers)
+      post(apiary_beekeepers_path(@apiary), payload.to_json, @http_headers)
 
       expect(response).to be_success
 
@@ -74,105 +76,226 @@ RSpec.describe 'Beekeepers', type: :request do
     end
 
     it 'will not allow users with write access to create beekeepers' do
-      # Create a user and and apiary and make that user an admin on that apiary
-      a_user = FactoryGirl.create(:user, email: 'a_user@example.com')
-
-      an_apiary = FactoryGirl.create(:apiary, user_id: a_user.id)
-      admin_beekeeper = FactoryGirl.create(:beekeeper,
-                                           user: a_user,
-                                           apiary: an_apiary,
-                                           creator: a_user.id)
-
-      # Create another user on that apiary, with write permissions
       another_user = create_logged_in_user(email: 'another_user@example.com')
       write_beekeeper = FactoryGirl.create(:beekeeper,
                                            user: another_user,
-                                           apiary: an_apiary,
-                                           creator: a_user.id,
+                                           apiary: @apiary,
+                                           creator: @user.id,
                                            permission: 'Write')
 
 
-      # Create a third user
       yet_another_user = FactoryGirl.create(:user, email: 'yet_another_user@example.com')
 
-      data = {
+      payload = {
         beekeeper: {
           email: yet_another_user.email,
           permission: 'Write'
         }
       }
 
-      post(apiary_beekeepers_path(an_apiary), data.to_json, @http_headers)
+      original_beek_count = Beekeeper.count
+
+      post(apiary_beekeepers_path(@apiary), payload.to_json, @http_headers)
       expect(response.code).to eq("401")
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['error']).to eq("You are not authorized to perform this action.")
+
+      expect(Beekeeper.count).to be(original_beek_count)
+    end
+
+    it 'will not allow users with read access to create beekeepers' do
+      another_user = create_logged_in_user(email: 'another_user@example.com')
+      write_beekeeper = FactoryGirl.create(:beekeeper,
+                                           user: another_user,
+                                           apiary: @apiary,
+                                           creator: @user.id,
+                                           permission: 'Write')
+
+      yet_another_user = FactoryGirl.create(:user, email: 'yet_another_user@example.com')
+
+      payload = {
+        beekeeper: {
+          email: yet_another_user.email,
+          permission: 'Read'
+        }
+      }
+
+      original_beek_count = Beekeeper.count
+
+      post(apiary_beekeepers_path(@apiary), payload.to_json, @http_headers)
+      expect(response.code).to eq("401")
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['error']).to eq("You are not authorized to perform this action.")
+
+      expect(Beekeeper.count).to be(original_beek_count)
+    end
+
+    it 'will not allow random users to create beekeepers at different apiaries' do
+      a_user = FactoryGirl.create(:user, email: 'a_user@example.com')
+      an_apiary = FactoryGirl.create(:apiary, user_id: a_user.id)
+
+      another_user = FactoryGirl.create(:user, email: 'another_user@example.com')
+
+      payload = {
+        beekeeper: {
+          email: another_user.email,
+          permission: 'Read'
+        }
+      }
+
+      original_beek_count = Beekeeper.count
+
+      post(apiary_beekeepers_path(an_apiary), payload.to_json, @http_headers)
+      expect(response.code).to eq("401")
+
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['error']).to eq("You are not authorized to perform this action.")
+
+      expect(Beekeeper.count).to be(original_beek_count)
+    end
+  end
+
+  describe '#update' do
+    it 'modifies an existing Beekeeper in the database' do
+      new_user = FactoryGirl.create(:user, email: 'another_user@example.com')
+      new_beekeeper = FactoryGirl.create(
+        :beekeeper,
+        user: new_user,
+        apiary: @apiary,
+        creator: @user.id,
+        permission: 'Read'
+      )
+
+      payload = {
+        beekeeper: {
+          permission: 'Write'
+        }
+      }
+
+      expect(new_beekeeper.permission).to eq('Read')
+
+      put(apiary_beekeeper_path(@apiary, new_beekeeper), payload.to_json, @http_headers)
+
+      expect(response).to be_success
+
+      new_beekeeper.reload
+
+      expect(new_beekeeper.permission).to eq('Write')
+
+      parsed_response = JSON.parse(response.body)
+
+      expect(parsed_response["id"]).to be(new_beekeeper.id)
+      expect(parsed_response["apiary_id"]).to be(@apiary.id)
+      expect(parsed_response["permission"]).to eq("Write")
+      expect(parsed_response["user"]["user_id"]).to be(new_user.id)
+      expect(parsed_response["user"]["first_name"]).to eq(new_user.first_name)
+      expect(parsed_response["user"]["last_name"]).to eq(new_user.last_name)
+    end
+
+    it 'will not allow users with write access to update beekeepers' do
+      write_user = create_logged_in_user(email: 'write_user@example.com')
+      write_beekeeper = FactoryGirl.create(
+        :beekeeper,
+        user: write_user,
+        apiary: @apiary,
+        creator: @user.id,
+        permission: 'Write'
+      )
+
+      another_user = FactoryGirl.create(:user, email: 'yet_another_user@example.com')
+      another_beekeeper = FactoryGirl.create(
+        :beekeeper,
+        user: another_user,
+        apiary: @apiary,
+        creator: @user.id,
+        permission: "Read"
+      )
+
+      payload = {
+        beekeeper: {
+          permission: 'Write'
+        }
+      }
+
+      expect(another_beekeeper.permission).to eq('Read')
+
+      put(apiary_beekeeper_path(@apiary, another_beekeeper), payload.to_json, @http_headers)
+      expect(response.code).to eq("401")
+
+      another_beekeeper.reload
+      expect(another_beekeeper.permission).to eq('Read')
 
       parsed_body = JSON.parse(response.body)
       expect(parsed_body['error']).to eq("You are not authorized to perform this action.")
     end
 
     it 'will not allow users with read access to create beekeepers' do
-      # Create a user and and apiary and make that user an admin on that apiary
-      a_user = FactoryGirl.create(:user, email: 'a_user@example.com')
+      read_user = create_logged_in_user(email: 'read_user@example.com')
+      write_beekeeper = FactoryGirl.create(
+        :beekeeper,
+        user: read_user,
+        apiary: @apiary,
+        creator: @user.id,
+        permission: 'Write'
+      )
 
-      an_apiary = FactoryGirl.create(:apiary, user_id: a_user.id)
-      admin_beekeeper = FactoryGirl.create(:beekeeper,
-                                           user: a_user,
-                                           apiary: an_apiary,
-                                           creator: a_user.id)
+      another_user = FactoryGirl.create(:user, email: 'another_user@example.com')
+      another_beekeeper = FactoryGirl.create(
+        :beekeeper,
+        user: another_user,
+        apiary: @apiary,
+        creator: @user.id,
+        permission: "Read"
+      )
 
-      # Create another user on that apiary, with write permissions
-      another_user = create_logged_in_user(email: 'another_user@example.com')
-      write_beekeeper = FactoryGirl.create(:beekeeper,
-                                           user: another_user,
-                                           apiary: an_apiary,
-                                           creator: a_user.id,
-                                           permission: 'Write')
-
-
-      # Create a third user
-      yet_another_user = FactoryGirl.create(:user, email: 'yet_another_user@example.com')
-
-      data = {
+      payload = {
         beekeeper: {
-          email: yet_another_user.email,
-          permission: 'Read'
+          permission: 'Write'
         }
       }
 
-      post(apiary_beekeepers_path(an_apiary), data.to_json, @http_headers)
+      expect(another_beekeeper.permission).to eq('Read')
+
+      put(apiary_beekeeper_path(@apiary, another_beekeeper), payload.to_json, @http_headers)
       expect(response.code).to eq("401")
+
+      another_beekeeper.reload
+      expect(another_beekeeper.permission).to eq('Read')
 
       parsed_body = JSON.parse(response.body)
       expect(parsed_body['error']).to eq("You are not authorized to perform this action.")
     end
 
-    it 'will not allow random users to create beekeepers at random apiaries' do
-      # Create a user and and apiary and make that user an admin on that apiary
+    it 'will not allow random users to edit beekeepers at different apiaries' do
       a_user = FactoryGirl.create(:user, email: 'a_user@example.com')
 
-      an_apiary = FactoryGirl.create(:apiary, user_id: a_user.id)
-      admin_beekeeper = FactoryGirl.create(:beekeeper,
-                                           user: a_user,
-                                           apiary: an_apiary,
-                                           creator: a_user.id)
+      different_apiary = FactoryGirl.create(:apiary, user_id: a_user.id)
+      a_beekeeper = FactoryGirl.create(
+        :beekeeper,
+        user: a_user,
+        apiary: different_apiary,
+        creator: a_user.id,
+        permission: 'Read'
+      )
 
-      # Create another user on that apiary, with write permissions
-      another_user = create_logged_in_user(email: 'another_user@example.com')
-
-      # Create a third user
-      yet_another_user = FactoryGirl.create(:user, email: 'yet_another_user@example.com')
-
-      data = {
+      payload = {
         beekeeper: {
-          email: yet_another_user.email,
-          permission: 'Read'
+          permission: 'Admin'
         }
       }
 
-      post(apiary_beekeepers_path(an_apiary), data.to_json, @http_headers)
-      expect(response.code).to eq("401")
+      expect(a_beekeeper.permission).to eq('Read')
+
+      put(apiary_beekeeper_path(different_apiary, a_beekeeper), payload.to_json, @http_headers)
+      expect(response.code).to eq('401')
 
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body['error']).to eq("You are not authorized to perform this action.")
+      expect(parsed_body['error']).to eq('You are not authorized to perform this action.')
+
+      a_beekeeper.reload
+      expect(a_beekeeper.permission).to eq('Read')
     end
   end
 end
