0a8242d2f742b37a18896e139d73d014479f7bc9
Brian Zable <brian@brianzable.com>
2014-09-17 21:40:41 -0500

    Add pending tests for Apiaries controller

--- spec/requests/apiaries_spec.rb
+++ spec/requests/apiaries_spec.rb
@@ -115,7 +115,34 @@ RSpec.describe 'Apiaries', type: :request do
 
   context 'json' do
     describe '#index' do
+      it "returns a list of the user's apiaries"
+    end
 
+    describe '#show' do
+      it 'allows users who are memebers of an apiary to view apiary information'
+      it 'does not allow users who are not members at an apiary to view an apiary'
+      it 'contains a list of hives associated with this apiary'
+    end
+
+    describe '#create' do
+      it 'creates and returns a JSON representation of the apiary'
+      it 'creates a beekeeper object making the creator an admin at the new apiary'
+    end
+
+    describe '#update' do
+      it 'allows the administator of an apiary to change apiary information'
+      it 'does not allow users with read permissions to update an apiary'
+      it 'does not allow users with write permissions to update an apiary'
+      it 'does not allow administrators of other apiaries to update other apiaries'
+    end
+
+    describe '#destroy' do
+      it 'allows admins at an apiary to delete an apiary'
+      it 'removes all beekeepers associated with this apiary'
+      it 'removes all hives associated with this apiary'
+      it 'does not allow users with read permissions to destroy an apiary'
+      it 'does not allow users with write permissions to destroy an apiary'
+      it 'does not allow users to destroy random apiaries'
     end
   end
 end
