600116fec9ce4ad5a33e8be018bfc521277f642d
Brian Zable <brian@brianzable.com>
2014-10-05 15:35:03 -0500

    Add tests for ApiryPolicy class

---
+++ spec/functional/policies/apiary_policy_spec.rb
@@ -0,0 +1,136 @@
+require 'rails_helper'
+
+describe 'ApiaryPolicy' do
+  before(:all) do
+    @user = FactoryGirl.build(:user, email: 'an_email@example.com')
+
+    @apiary = FactoryGirl.build(
+      :apiary,
+      user_id: @user.id
+    )
+
+    @admin_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'admin_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id
+    )
+
+    @write_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'write_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Write'
+    )
+
+    @read_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'read_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Read'
+    )
+  end
+  describe 'index?' do
+    it 'returns true for beekeepers with read permissions' do
+      policy = ApiaryPolicy.new(@read_beekeeper, @apiary)
+      expect(policy.index?).to be(true)
+    end
+
+    it 'returns true for beekeepers with write permissions' do
+      policy = ApiaryPolicy.new(@write_beekeeper, @apiary)
+      expect(policy.index?).to be(true)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = ApiaryPolicy.new(@admin_beekeeper, @apiary)
+      expect(policy.index?).to be(true)
+    end
+  end
+
+  describe 'new?' do
+    it 'returns true for beekeepers with read permissions' do
+      policy = ApiaryPolicy.new(@read_beekeeper, @apiary)
+      expect(policy.new?).to be(true)
+    end
+
+    it 'returns true for beekeepers with write permissions' do
+      policy = ApiaryPolicy.new(@write_beekeeper, @apiary)
+      expect(policy.new?).to be(true)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = ApiaryPolicy.new(@admin_beekeeper, @apiary)
+      expect(policy.new?).to be(true)
+    end
+  end
+
+  describe 'create?' do
+    it 'returns true for beekeepers with read permissions' do
+      policy = ApiaryPolicy.new(@read_beekeeper, @apiary)
+      expect(policy.create?).to be(true)
+    end
+
+    it 'returns true for beekeepers with write permissions' do
+      policy = ApiaryPolicy.new(@write_beekeeper, @apiary)
+      expect(policy.create?).to be(true)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = ApiaryPolicy.new(@admin_beekeeper, @apiary)
+      expect(policy.create?).to be(true)
+    end
+  end
+
+  describe 'edit?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = ApiaryPolicy.new(@read_beekeeper, @apiary)
+      expect(policy.edit?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = ApiaryPolicy.new(@write_beekeeper, @apiary)
+      expect(policy.edit?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = ApiaryPolicy.new(@admin_beekeeper, @apiary)
+      expect(policy.edit?).to be(true)
+    end
+  end
+
+  describe 'update?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = ApiaryPolicy.new(@read_beekeeper, @apiary)
+      expect(policy.update?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = ApiaryPolicy.new(@write_beekeeper, @apiary)
+      expect(policy.update?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = ApiaryPolicy.new(@admin_beekeeper, @apiary)
+      expect(policy.update?).to be(true)
+    end
+  end
+
+  describe 'destroy?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = ApiaryPolicy.new(@read_beekeeper, @apiary)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = ApiaryPolicy.new(@write_beekeeper, @apiary)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = ApiaryPolicy.new(@admin_beekeeper, @apiary)
+      expect(policy.destroy?).to be(true)
+    end
+  end
+end
