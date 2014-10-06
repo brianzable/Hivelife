eaa1ee1b131bdc7eee1430f3b065b98bc555b687
Brian Zable <brian@brianzable.com>
2014-10-05 19:11:00 -0500

    Add tests for BeekeeperPolicy

---
+++ spec/functional/policies/beekeeper_policy_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+describe 'BeekeeperPolicy' do
+  before(:all) do
+    @user = FactoryGirl.build(:user, email: 'an_email@example.com')
+
+    @apiary = FactoryGirl.build(
+      :apiary,
+      user_id: @user.id
+    )
+
+    @admin_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'admin_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id
+    )
+
+    @write_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'write_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Write'
+    )
+
+    @read_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'read_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Read'
+    )
+
+    @new_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'another_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Admin'
+    )
+  end
+
+  describe 'create?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = BeekeeperPolicy.new(@read_beekeeper, @new_beekeeper)
+      expect(policy.create?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = BeekeeperPolicy.new(@write_beekeeper, @new_beekeeper)
+      expect(policy.create?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = BeekeeperPolicy.new(@admin_beekeeper, @new_beekeeper)
+      expect(policy.create?).to be(true)
+    end
+  end
+
+  describe 'update?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = BeekeeperPolicy.new(@read_beekeeper, @new_beekeeper)
+      expect(policy.update?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = BeekeeperPolicy.new(@write_beekeeper, @new_beekeeper)
+      expect(policy.update?).to be(false)
+    end
+
+    it 'returns false or beekeepers trying to update an admin' do
+      policy = BeekeeperPolicy.new(@admin_beekeeper, @new_beekeeper)
+      expect(policy.update?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions who are not trying to update an admin' do
+      policy = BeekeeperPolicy.new(@admin_beekeeper, @write_beekeeper)
+      expect(policy.update?).to be(true)
+    end
+  end
+
+  describe 'destroy?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = BeekeeperPolicy.new(@read_beekeeper, @new_beekeeper)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = BeekeeperPolicy.new(@write_beekeeper, @new_beekeeper)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns false or beekeepers trying to update an admin' do
+      policy = BeekeeperPolicy.new(@admin_beekeeper, @new_beekeeper)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions who are not trying to update an admin' do
+      policy = BeekeeperPolicy.new(@admin_beekeeper, @write_beekeeper)
+      expect(policy.destroy?).to be(true)
+    end
+
+    it 'returns true for beekeepers attemping to destroy themselves' do
+      policy = BeekeeperPolicy.new(@read_beekeeper, @read_beekeeper)
+      expect(policy.destroy?).to be(true)
+    end
+  end
+end
