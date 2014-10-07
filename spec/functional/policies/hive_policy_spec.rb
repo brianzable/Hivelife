838187a441becc5490b9d12752f30ce594cdd75d
Brian Zable <brian@brianzable.com>
2014-10-06 20:22:49 -0500

    Add tests to HivePolicy

---
+++ spec/functional/policies/hive_policy_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+describe 'HivePolicy' do
+  before(:all) do
+    @user = FactoryGirl.build(:user, email: 'an_email@example.com')
+
+    @apiary = FactoryGirl.build(
+      :apiary_with_hives,
+      user_id: @user.id
+    )
+
+    @admin_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'admin_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id
+    )
+
+    @write_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'write_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Write'
+    )
+
+    @read_beekeeper = FactoryGirl.build(
+      :beekeeper,
+      user: FactoryGirl.build(:user, email: 'read_user@example.com'),
+      apiary: @apiary,
+      creator: @user.id,
+      permission: 'Read'
+    )
+
+    @hive = @apiary.hives.first
+  end
+
+  describe 'show?' do
+    it 'returns true for beekeepers with read permissions' do
+      policy = HivePolicy.new(@read_beekeeper, @hive)
+      expect(policy.show?).to be(true)
+    end
+
+    it 'returns true for beekeepers with write permissions' do
+      policy = HivePolicy.new(@write_beekeeper, @hive)
+      expect(policy.show?).to be(true)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = HivePolicy.new(@admin_beekeeper, @hive)
+      expect(policy.show?).to be(true)
+    end
+  end
+
+  describe 'create?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = HivePolicy.new(@read_beekeeper, @hive)
+      expect(policy.create?).to be(false)
+    end
+
+    it 'returns true for beekeepers with write permissions' do
+      policy = HivePolicy.new(@write_beekeeper, @hive)
+      expect(policy.create?).to be(true)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = HivePolicy.new(@admin_beekeeper, @hive)
+      expect(policy.create?).to be(true)
+    end
+  end
+
+  describe 'update?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = HivePolicy.new(@read_beekeeper, @hive)
+      expect(policy.update?).to be(false)
+    end
+
+    it 'returns true for beekeepers with write permissions' do
+      policy = HivePolicy.new(@write_beekeeper, @hive)
+      expect(policy.update?).to be(true)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = HivePolicy.new(@admin_beekeeper, @hive)
+      expect(policy.update?).to be(true)
+    end
+  end
+
+  describe 'destroy?' do
+    it 'returns false for beekeepers with read permissions' do
+      policy = HivePolicy.new(@read_beekeeper, @hive)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns false for beekeepers with write permissions' do
+      policy = HivePolicy.new(@write_beekeeper, @hive)
+      expect(policy.destroy?).to be(false)
+    end
+
+    it 'returns true for beekeepers with admin permissions' do
+      policy = HivePolicy.new(@admin_beekeeper, @hive)
+      expect(policy.destroy?).to be(true)
+    end
+  end
+end
+
