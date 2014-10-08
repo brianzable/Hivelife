c13c7411bb5871f3f21d2fd2adbde0117fbce979
Brian Zable <brian@brianzable.com>
2014-10-07 21:40:34 -0500

    Add tests for Beekeeper model

---
+++ spec/functional/models/beekeeper_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+describe Beekeeper do
+  describe 'admin?' do
+    it 'returns true if the permission is Admin' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Admin')
+      expect(beekeeper.admin?).to be(true)
+    end
+
+    it 'returns false if the permission is Read' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Read')
+      expect(beekeeper.admin?).to be(false)
+    end
+
+    it 'returns false if the permission is Write' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Write')
+      expect(beekeeper.admin?).to be(false)
+    end
+  end
+
+  describe 'read?' do
+    it 'returns true if the permission is Admin' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Admin')
+      expect(beekeeper.read?).to be(true)
+    end
+
+    it 'returns true if the permission is Read' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Read')
+      expect(beekeeper.read?).to be(true)
+    end
+
+    it 'returns true if the permission is Write' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Write')
+      expect(beekeeper.read?).to be(true)
+    end
+  end
+
+  describe 'write?' do
+    it 'returns true if the permission is Admin' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Write')
+      expect(beekeeper.write?).to be(true)
+    end
+
+    it 'returns false if the permission is Read' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Read')
+      expect(beekeeper.write?).to be(false)
+    end
+
+    it 'returns true if the permission is Write' do
+      beekeeper = FactoryGirl.build(:beekeeper, permission: 'Write')
+      expect(beekeeper.write?).to be(true)
+    end
+  end
+end
+
