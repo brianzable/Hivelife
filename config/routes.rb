174f7a87a90148ed6094374e209d4cf927dd378f
Brian Zable <brian@brianzable.com>
2014-11-29 19:08:05 -0600

    Redirect all requests to the root URL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Bees::Application.routes.draw do
-  authenticated :user do
-    root to: 'apiaries#index', as: 'authenticated_root'
-  end
-
+  get '*path' => redirect('/')
+  # authenticated :user do
+  #   root to: 'apiaries#index', as: 'authenticated_root'
+  # end
+  #
   root 'static_pages#index'
-
-  get '/data', to: 'static_pages#data'
-
-  resources :apiaries do
-  	resources :hives, except: [:index]
-    resources :beekeepers, except: [:index, :new, :edit] do
-      post 'preview', on: :new
-    end
-  end
-
-  resources :hives do
-  	resources :inspections, except: [:index]
-  	resources :harvests, except: [:index]
-  end
-
-  devise_for :users
+  #
+  # get '/data', to: 'static_pages#data'
+  #
+  # resources :apiaries do
+  # 	resources :hives, except: [:index]
+  #   resources :beekeepers, except: [:index, :new, :edit] do
+  #     post 'preview', on: :new
+  #   end
+  # end
+  #
+  # resources :hives do
+  # 	resources :inspections, except: [:index]
+  # 	resources :harvests, except: [:index]
+  # end
+  #
+  # devise_for :users
 end
