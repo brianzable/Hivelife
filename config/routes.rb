d87532b25ed4d48bbf7d84d2fecca024da679e17
Brian Zable <brian@brianzable.com>
2015-05-31 14:11:49 -0500

    Move all routes under an API scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Bees::Application.routes.draw do
-  root 'static_pages#index'
+  scope :api do
+    get '/data', to: 'static_pages#data'
+    post '/sign_in', to: 'sessions#sign_in'
 
-  get '/data', to: 'static_pages#data'
-  post '/sign_in', to: 'sessions#sign_in'
-
-  resources :apiaries do
-  	resources :hives, except: [:index]
-    resources :beekeepers, except: [:index, :new, :edit] do
-      post 'preview', on: :new
+    resources :apiaries do
+      resources :hives, except: [:index]
+      resources :beekeepers, except: [:index, :new, :edit] do
+        post 'preview', on: :new
+      end
     end
-  end
 
-  resources :hives do
-  	resources :inspections, except: [:index]
-  	resources :harvests, except: [:index]
-  end
+    resources :hives do
+      resources :inspections, except: [:index]
+      resources :harvests, except: [:index]
+    end
 
-  get '/users/:activation_token/activate', to: 'users#activate'
-  resources :users, except: [:index, :new, :edit]
+    get '/users/:activation_token/activate', to: 'users#activate'
+    resources :users, except: [:index, :new, :edit]
+  end
 end
