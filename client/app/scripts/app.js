271194d95c8c4af9b2ddfd48f29f7bd30fa6e34f
Brian Zable <brian@brianzable.com>
2015-12-20 20:16:25 -0600

    Update menu when session updates

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,27 +1,25 @@
 (function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
   app.displayInstalledToast = function() {
     document.querySelector('#caching-complete').show();
   };
 
+  app.updateSession = function (context, next) {
+    if (app.session === null || typeof app.session === undefined) {
+      app.authenticationToken = null;
+    } else {
+      app.authenticationToken = app.session.authentication_token;
+    }
+
+    app.$.sessionStore.value = app.session;
+    app.$.rootManager.reloadMenu();
+    next();
+  };
+
   app.addEventListener('dom-change', function() {
-    app.$.sessionStore.addEventListener('value-changed', function (event) {
-      var session = event.detail.value;
-      if (session === null) {
-        app.authenticationToken = null;
-      } else {
-        app.authenticationToken = session.authentication_token;
-      }
-
-      app.$.rootManager.reloadMenu();
-    });
-
     app.$.pages.addEventListener('page-redirect', function (event) {
       page.show(event.detail.path);
     });
