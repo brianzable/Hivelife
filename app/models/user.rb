baa64e75fc402c12119e786f05eaee637460ce2f
Brian Zable <brian@brianzable.com>
2016-02-06 19:22:07 -0600

    Last minute fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   before_create :set_authentication_token
+  before_save :set_photo_url
 
   validates :password, length: { minimum: 8 }, if: :validate_password?
   validates :password, confirmation: true, if: :validate_password?
   validates :password_confirmation, presence: true, if: :validate_password?
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 
   def validate_password?
     new_record? || password.present?
@@ -26,5 +27,10 @@ class User < ActiveRecord::Base
 
   def generate_authentication_token
     SecureRandom.uuid.gsub(/\-/,'')
+  end
+
+  def set_photo_url
+    md5 = Digest::MD5.hexdigest(self.email.downcase)
+    self.photo_url = "http://www.gravatar.com/avatar/#{md5}?s=200"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe User, type: :model do
       expect(user.authentication_token).to_not be_nil
     end
   end
+
+  describe 'photo_url' do
+    it 'saves the Gravatar photo URL on create' do
+      user = User.new(
+        email: 'user@example.com',
+        password: '11111111',
+        password_confirmation: '11111111'
+      )
+      expect(user.photo_url).to be_nil
+
+      user.save!
+      expect(user.photo_url).to eq('http://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?s=200')
+    end
+  end
 end
