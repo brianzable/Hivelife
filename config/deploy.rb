6e1c476a804bfecff801d0830ddf2165a086e4bf
Brian Zable <brian@brianzable.com>
2016-03-28 22:03:29 -0500

    More changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,8 +89,8 @@ set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{shared_path}/logs/puma.error.log"
-set :puma_error_log,  "#{shared_path}/logs/puma.access.log"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -112,7 +112,6 @@ namespace :puma do
     on roles(:app) do
       execute "mkdir #{shared_path}/tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
-      execute "mkdir #{shared_path}/logs -p"
     end
   end
 
