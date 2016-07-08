9119287b5eb453dbb8191d3d073b8bdd790c24fe
Brian Zable <brian@brianzable.com>
2016-07-08 11:33:34 -0500

    adjust base URL

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def email_link(path)
-    "http://ec2-54-213-211-214.us-west-2.compute.amazonaws.com#{path}"
+    "http://ec2-54-201-165-158.us-west-2.compute.amazonaws.com#{path}"
   end
 end
